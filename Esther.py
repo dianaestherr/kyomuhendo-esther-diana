825438bfab04d569d0a5c145b2448dbd9f72d229
dianaestherr <kyomuhendo.diana.esther@gmail.com>
2017-05-18 18:11:10 +0300

    Add files via upload

---
+++ Esther.py
@@ -0,0 +1,10 @@
+import calendar #imports the calendar module from the python modules
+from datetime import datetime
+now = datetime.now()#gets the current date and time
+age = int(input("Enter your age: "))
+year = int(input('Enter year:'))
+month = int(input("Enter the month: "))
+day = int(input("Enter the day of the month: "))
+cal = calendar.weekday(year,month,day)#calculates the day of the week
+days = {0:'Monday',1:'Tuesday',2:'Wednesday',3:'Thursday',4:'Friday',5:'Saturday',6:'Sunday'}
+print('You were born on',days[cal])#final statement that prints the day of week
